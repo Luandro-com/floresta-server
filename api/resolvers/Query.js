4f238e3e4be67d338d4ab2f08a790c771488e65d
luandro <luandro@gmail.com>
2019-06-03 16:35:17 -0300

    working API

--- api/resolvers/Query.js
+++ api/resolvers/Query.js
@@ -1,9 +1,19 @@
-const { getUserId } = require('../services/auth/utils')
+const {
+  getUserId
+} = require('../services/auth/utils')
 // const fetch = require('node-fetch')
 
 const Query = {
+  async content(parent, args, ctx, info) {
+    const res = await ctx.db.query.contents({}, info)
+    return res[0]
+  },
   feed(parent, args, ctx, info) {
-    return ctx.db.query.posts({ where: { isPublished: true } }, info)
+    return ctx.db.query.posts({
+      where: {
+        isPublished: true
+      }
+    }, info)
   },
 
   drafts(parent, args, ctx, info) {
@@ -16,17 +26,57 @@ const Query = {
       }
     }
 
-    return ctx.db.query.posts({ where }, info)
+    return ctx.db.query.posts({
+      where
+    }, info)
   },
 
-  post(parent, { id }, ctx, info) {
-    return ctx.db.query.post({ where: { id } }, info)
+  post(parent, {
+    id
+  }, ctx, info) {
+    return ctx.db.query.post({
+      where: {
+        id
+      }
+    }, info)
+  },
+
+  async admins(parent, args, ctx, info) {
+    return await ctx.db.query.users({ where: { role_in: ['ADMIN', 'EDITOR'] } }, info)
   },
 
   user(parent, args, ctx, info) {
     const id = getUserId(ctx)
-    return ctx.db.query.user({ where: { id } }, info)
+    return ctx.db.query.user({
+      where: {
+        id
+      }
+    }, info)
+  },
+  // projectCategories(parent, args, ctx, info) {
+  //   return ctx.db.query.projectCategories(null, info)
+  // },
+
+  project(parent, args, ctx, info) {
+    const { id, slug} = args
+    if (id) {
+      return ctx.db.query.project({ where: { id }}, info)
+    } else if (slug) {
+      return ctx.db.query.project({ where: { slug }}, info)
+    }
+  },
+
+  projects(parent, args, ctx, info) {
+    const { id, slug, category, subCategory } = args
+    if (category) {
+      return ctx.db.query.projects({ where: { category: { slug: category } }}, info)
+    } else if (subCategory) {
+      return ctx.db.query.projects({ where: { subCategories: { slug: subCategory } }}, info)
+    }
+    return ctx.db.query.projects(null, info)
   },
 }
 
-module.exports = { Query }
+module.exports = {
+  Query
+}
--- api/resolvers/index.js
+++ api/resolvers/index.js
@@ -1,28 +1,28 @@
-const { Query } = require('./Query')
-const { GraphQLUpload } = require('graphql-upload')
-const { Subscription } = require('./Subscription')
-const { auth } = require('./Mutation/auth')
-const { post } = require('./Mutation/post')
-// const { admin } = require('./Mutation/admin')
+const { Query } = require('./Query');
+const { GraphQLUpload } = require('graphql-upload');
+const { Subscription } = require('./Subscription');
+const { auth } = require('./Mutation/auth');
+const { post } = require('./Mutation/post');
+const { admin } = require('./Mutation/admin');
 // const { shop } = require('./Mutation/shop')
-const { upload } = require('./Mutation/upload')
-const { AuthPayload } = require('./AuthPayload')
+const { upload } = require('./Mutation/upload');
+const { AuthPayload } = require('./AuthPayload');
 
 module.exports = {
-  Query,
-  Mutation: {
-    ...auth,
-    ...post,
-    // ...admin,
-    // ...shop,
-    ...upload,
-  },
-  Subscription,
-  Upload: GraphQLUpload,
-  AuthPayload,
-  Node: {
-    __resolveType() {
-      return null;
-    }
-  }
-}
+	Query,
+	Mutation: {
+		...auth,
+		...post,
+		...admin,
+		// ...shop,
+		...upload
+	},
+	Subscription,
+	Upload: GraphQLUpload,
+	AuthPayload,
+	Node: {
+		__resolveType() {
+			return null;
+		}
+	}
+};
