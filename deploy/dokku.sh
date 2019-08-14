f782ffcf636a6e78eba910527b18be114dcfb20f
luandro <luandro@gmail.com>
2019-08-14 13:22:49 -0300

    added env vars to dokku script and removed from Dockerfile

--- deploy/dokku.sh
+++ deploy/dokku.sh
@@ -9,7 +9,20 @@ dokku apps:create floresta-web
 sudo dokku plugin:install https://github.com/dokku/dokku-mysql.git
 dokku mysql:create florestaprotegida
 dokku mysql:link florestaprotegida floresta-prisma-server
-dokku config:set floresta-prisma-server DATABASE_URL=mysql.$HOSTDOMAIN DATABASE_PASSWORD=$PASSWORD
+dokku config:set floresta-prisma-server \
+DATABASE_URL=mysql.$HOSTDOMAIN
+DATABASE_PASSWORD=$PASSWORD \
+NODE_ENV="production" \
+PRODUCTION="true" \
+PRISMA_STAGE="production" \
+PRISMA_ENDPOINT="https://us1.prisma.sh/luandro-93a3b2/florestaprotegida/dev" \
+PRISMA_SECRET="mysecret123" \
+PRISMA_MANAGEMENT_API_SECRET='mysupersecret123' \
+APP_SECRET="jwtsecret123" \
+S3_BUCKET_NAME="florestaprotegida" \
+S3_AWS_SECRET_ACCESS_KEY="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" \
+S3_AWS_ACCESS_KEY_ID="AKIAIOSFODNN7EXAMPLE" \
+S3_ENDPOINT="http://165.227.110.74:9000/"
 dokku docker-options:add floresta-prisma-server build '--file prisma.dockerfile'
 
 echo "\n\n\n\nMysql password: ${PASSWORD}"
